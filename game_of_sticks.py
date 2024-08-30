8b413c29f76373b539e800b1d9b3d50edfd1ec06
Sophia Krause-Levy <skrauselevy@sandiego.edu>
2024-08-30 10:14:17 -0700

    add starter code

---
+++ game_of_sticks.py
@@ -0,0 +1,46 @@
+"""
+Module: game_of_sticks
+
+Implementation of the Game of Sticks, including an AI that learns the game,
+either by playing against a human, or by pre-training against another AI.
+
+Authors:
+1) Name - USD Email Address
+2) Name - USD Email Address
+"""
+
+import random
+
+
+def get_player_selection(player_number, sticks_left):
+    """
+    Replace this fake docstring comment with a real one with the proper
+    formatting.
+
+    It should be the very first thing you do when writing this function.
+    """
+
+    pass # remove this line when you start implementing your function
+
+
+def player_vs_player(num_sticks):
+    """
+    Replace this fake docstring comment with a real one with the proper
+    formatting.
+
+    It should be the very first thing you do when writing this function.
+    """
+
+    pass # remove this line when you start implementing your function
+
+
+def main():
+    print("Welcome to the Game of Sticks!")
+    num_sticks = int(input("How many sticks are there on the table initially (10-100)? "))
+
+    player_vs_player(num_sticks)
+
+
+if __name__ == "__main__":
+    main()
+
